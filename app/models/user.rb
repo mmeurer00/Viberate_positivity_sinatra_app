0f53272a52f911f9982f4b583a11767ff4fa5398
Maxine <maxinemeurer@yahoo.com>
2021-03-24 15:25:52 -0400

    added in follows table, and established relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
     has_secure_password
+
     has_many :posts
+
+    has_many :follows
+
+    has_many :follower_relationships, foreign_key: :following_id, class_name: "Follow"
+    has_many :followers, through: :follower_relationships, source: :follower
+
+    has_many :following_relationships, foreign_key: :user_id, class_name: "Follow"
+    has_many :following, through: :following_relationships, source: :following
+
     validates :user_name, presence: true
     validates :user_name, uniqueness: { case_sensitive: false }
     validates :email, presence: true
+
     def slug
       user_name.downcase.gsub(" ","-")
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_160142) do
+ActiveRecord::Schema.define(version: 2021_03_24_191524) do
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
